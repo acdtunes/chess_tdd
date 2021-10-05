82280fe47a555928c3ad220a13a908e257a934fa
Andres Camacho <andres@Andress-MacBook-Air.local>
2021-10-05 13:39:42 -0300

    refactorings

--- board.go
+++ board.go
@@ -9,6 +9,7 @@ type Board struct {
 	size           int
 	pos            string
 	piece          string
+	moves		   []string
 }
 
 func (b *Board) PlacePiece(piece string, pos string) {
@@ -18,123 +19,68 @@ func (b *Board) PlacePiece(piece string, pos string) {
 
 func (b *Board) Moves() []string {
 	if b.piece == "R" {
-		return b.straightMoves()
+		b.addStraightMoves()
 	}
 	if b.piece == "B" {
-		return b.diagonalMoves()
+		b.addDiagonalMoves()
 	}
 	if b.piece == "Q" {
-		return b.straightAndDiagonalMoves()
+		b.addStraightAndDiagonalMoves()
 	}
 	if b.piece == "K" {
-		return b.knightMoves()
+		b.addKnightMoves()
 	}
 
-	return []string{}
+	return b.moves
 }
 
-func (b *Board) knightMoves() []string {
-	var moves []string
-
-	if b.pieceCol()-2 >= 1 {
-		if b.pieceLine()+1 <= b.size {
-			moves = append(moves, getPosition(b.pieceCol()-2, b.pieceLine()+1))
-		}
-		if b.pieceLine()-1 >= 1 {
-			moves = append(moves, getPosition(b.pieceCol()-2, b.pieceLine()-1))
-		}
-	}
-	if b.pieceCol()-1 >= 1 {
-		if b.pieceLine()+2 <= b.size {
-			moves = append(moves, getPosition(b.pieceCol()-1, b.pieceLine()+2))
-		}
-		if b.pieceLine()-2 >= 1 {
-			moves = append(moves, getPosition(b.pieceCol()-1, b.pieceLine()-2))
-		}
-	}
-	if b.pieceCol()+2 <= b.size {
-		if b.pieceLine()+1 <= b.size {
-			moves = append(moves, getPosition(b.pieceCol()+2, b.pieceLine()+1))
-		}
-		if b.pieceLine()-1 >= 1 {
-			moves = append(moves, getPosition(b.pieceCol()+2, b.pieceLine()-1))
-		}
-	}
-	if b.pieceCol()+1 <= b.size {
-		if b.pieceLine()+2 <= b.size {
-			moves = append(moves, getPosition(b.pieceCol()+1, b.pieceLine()+2))
-		}
-		if b.pieceLine()-2 >= 1 {
-			moves = append(moves, getPosition(b.pieceCol()+1, b.pieceLine()-2))
-		}
-	}
-
-	return moves
+func (b *Board) addStraightAndDiagonalMoves() {
+	b.addStraightMoves()
+	b.addDiagonalMoves()
 }
 
-func (b *Board) straightAndDiagonalMoves() []string {
-	moves := b.straightMoves()
-	moves = append(moves, b.diagonalMoves()...)
-	return moves
+func (b *Board) addStraightMoves() {
+	for col := 1; col <= b.size; col++ {
+		b.addMove(col, b.pieceLine())
+	}
+	for line := 1; line <= b.size; line++ {
+		b.addMove(b.pieceCol(), line)
+	}
 }
 
-func (b *Board) straightMoves() []string {
-	var moves []string
-
-	for c := 1; c <= b.size; c++ {
-		if !b.pieceIsOnSamePosition(c, b.pieceLine()) {
-			moves = append(moves, getPosition(c, b.pieceLine()))
-		}
-	}
-	for l := 1; l <= b.size; l++ {
-		if !b.pieceIsOnSamePosition(b.pieceCol(), l) {
-			moves = append(moves, getPosition(b.pieceCol(), l))
-		}
+func (b *Board) addDiagonalMoves() {
+	for col := b.pieceCol() + 1; col <= b.size; col++ {
+		b.addMove(col, b.diagonalAboveLine(col))
+		b.addMove(col, b.diagonalBelowLine(col))
 	}
 
-	return moves
-}
-
-func (b *Board) diagonalMoves() []string {
-	var moves []string
-
-	for c := b.pieceCol() + 1; c <= b.size; c++ {
-		upper := b.upperLine(c)
-		if b.isInBounds(upper) {
-			moves = append(moves, getPosition(c, upper))
-		}
-
-		lower := b.lowerLine(c)
-		if b.isInBounds(lower) {
-			moves = append(moves, getPosition(c, lower))
-		}
+	for col := b.pieceCol() - 1; col >= 1; col-- {
+		b.addMove(col, b.diagonalAboveLine(col))
+		b.addMove(col, b.diagonalBelowLine(col))
 	}
-
-	for c := b.pieceCol() - 1; c >= 1; c-- {
-		upper := b.upperLine(c)
-		if b.isInBounds(upper) {
-			moves = append(moves, getPosition(c, upper))
-		}
-
-		lower := b.lowerLine(c)
-		if b.isInBounds(lower) {
-			moves = append(moves, getPosition(c, lower))
-		}
-	}
-
-	return moves
 }
 
-func (b *Board) lowerLine(c int) int {
-	return b.pieceLine() - (b.pieceCol() - c)
+func (b *Board) diagonalBelowLine(col int) int {
+	return b.pieceLine() - (b.pieceCol() - col)
 }
 
-func (b *Board) upperLine(c int) int {
-	return b.pieceLine() + (b.pieceCol() - c)
+func (b *Board) diagonalAboveLine(col int) int {
+	return b.pieceLine() + (b.pieceCol() - col)
 }
 
-func (b *Board) isInBounds(x int) bool {
-	return x >= 1 && x <= b.size
+func (b *Board) isInBounds(p int) bool {
+	return p >= 1 && p <= b.size
+}
+
+func (b *Board) addKnightMoves() {
+	b.addMove(b.pieceCol()-2, b.pieceLine()+1)
+	b.addMove(b.pieceCol()-2, b.pieceLine()-1)
+	b.addMove(b.pieceCol()+2, b.pieceLine()+1)
+	b.addMove(b.pieceCol()+2, b.pieceLine()-1)
+	b.addMove(b.pieceCol()-1, b.pieceLine()+2)
+	b.addMove(b.pieceCol()-1, b.pieceLine()-2)
+	b.addMove(b.pieceCol()+1, b.pieceLine()+2)
+	b.addMove(b.pieceCol()+1, b.pieceLine()-2)
 }
 
 func (b *Board) pieceLine() int {
@@ -146,17 +92,21 @@ func (b *Board) pieceCol() int {
 	return int(b.pos[0]) - 96
 }
 
-func (b *Board) pieceIsOnSamePosition(c int, l int) bool {
-	return getPosition(c, l) == b.pos
+func (b *Board) addMove(col int, line int) {
+	if !b.pieceIsOnSamePosition(col, line) && b.isInBounds(col) && b.isInBounds(line) {
+		b.moves = append(b.moves, getPosition(col, line))
+	}
 }
 
-func getPosition(c int, l int) string {
-	return fmt.Sprintf("%s%d", string(c+96), l)
+func (b *Board) pieceIsOnSamePosition(col int, line int) bool {
+	return getPosition(col, line) == b.pos
+}
+
+func getPosition(col int, line int) string {
+	return fmt.Sprintf("%c%d", rune(col+96), line)
 }
 
 func NewBoardOfSize(size int) *Board {
-	b := &Board{size: size}
-
-	return b
+	return &Board{size: size, moves: []string{}}
 }
 
--- board_test.go
+++ board_test.go
@@ -3,6 +3,7 @@ package chess
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
 	"testing"
 )
 
@@ -132,92 +133,90 @@ var _ = Describe("Chess", func() {
 				})
 			})
 		})
-		Context("Rook", func() {
-			Context("8x8", func() {
-				Context("Bishop on d4", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(8)
-						board.PlacePiece("B", "d4")
-						Expect(board.Moves()).To(ConsistOf("e5", "f6", "g7", "h8", "c3", "b2", "a1",
-							"e3", "f2", "g1", "c5", "b6", "a7"))
-					})
+	})
+	Context("Bishop", func() {
+		Context("1x1", func() {
+			Context("Bishop on a1", func() {
+				It("returns empty list", func() {
+					board := NewBoardOfSize(1)
+					board.PlacePiece("B", "a1")
+					Expect(board.Moves()).To(HaveLen(0))
 				})
 			})
 		})
-		Context("Bishop", func() {
-			Context("1x1", func() {
-				Context("Bishop on a1", func() {
-					It("returns empty list", func() {
-						board := NewBoardOfSize(1)
-						board.PlacePiece("B", "a1")
-						Expect(board.Moves()).To(HaveLen(0))
-					})
+		Context("2x2", func() {
+			Context("Bishop on a1", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(2)
+					board.PlacePiece("B", "a1")
+					Expect(board.Moves()).To(ConsistOf("b2"))
 				})
 			})
-			Context("2x2", func() {
-				Context("Bishop on a1", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(2)
-						board.PlacePiece("B", "a1")
-						Expect(board.Moves()).To(ConsistOf("b2"))
-					})
-				})
-				Context("Bishop on a2", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(2)
-						board.PlacePiece("B", "a2")
-						Expect(board.Moves()).To(ConsistOf("b1"))
-					})
-				})
-				Context("Bishop on b1", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(2)
-						board.PlacePiece("B", "b1")
-						Expect(board.Moves()).To(ConsistOf("a2"))
-					})
-				})
-				Context("Bishop on b2", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(2)
-						board.PlacePiece("B", "b2")
-						Expect(board.Moves()).To(ConsistOf("a1"))
-					})
+			Context("Bishop on a2", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(2)
+					board.PlacePiece("B", "a2")
+					Expect(board.Moves()).To(ConsistOf("b1"))
 				})
 			})
-			Context("3x3", func() {
-				Context("Bishop on a1", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(3)
-						board.PlacePiece("B", "a1")
-						Expect(board.Moves()).To(ConsistOf("b2", "c3"))
-					})
+			Context("Bishop on b1", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(2)
+					board.PlacePiece("B", "b1")
+					Expect(board.Moves()).To(ConsistOf("a2"))
 				})
 			})
-			Context("8x8", func() {
-				Context("Bishop on b3", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(8)
-						board.PlacePiece("B", "b3")
-						Expect(board.Moves()).To(ConsistOf("a4", "a2", "c2", "d1", "c4", "d5", "e6", "f7", "g8"))
-					})
+			Context("Bishop on b2", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(2)
+					board.PlacePiece("B", "b2")
+					Expect(board.Moves()).To(ConsistOf("a1"))
 				})
 			})
 		})
-		Context("Knight", func() {
-			Context("8x8", func() {
-				Context("Knight on b1", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(8)
-						board.PlacePiece("K", "b1")
-						Expect(board.Moves()).To(ConsistOf("a3", "c3", "d2"))
-					})
+		Context("3x3", func() {
+			Context("Bishop on a1", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(3)
+					board.PlacePiece("B", "a1")
+					Expect(board.Moves()).To(ConsistOf("b2", "c3"))
 				})
-				Context("Knight on c4", func() {
-					It("returns correct positions", func() {
-						board := NewBoardOfSize(8)
-						board.PlacePiece("K", "c4")
-						Expect(board.Moves()).To(ConsistOf("e5", "d6", "d2", "e3", "b2", "a3", "a5", "b6"))
-					})
+			})
+		})
+		Context("8x8", func() {
+			Context("Bishop on b3", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(8)
+					board.PlacePiece("B", "b3")
+					Expect(board.Moves()).To(ConsistOf("a4", "a2", "c2", "d1", "c4", "d5", "e6", "f7", "g8"))
+				})
+			})
+		})
+		Context("8x8", func() {
+			Context("Bishop on d4", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(8)
+					board.PlacePiece("B", "d4")
+					Expect(board.Moves()).To(ConsistOf("e5", "f6", "g7", "h8", "c3", "b2", "a1",
+						"e3", "f2", "g1", "c5", "b6", "a7"))
+				})
+			})
+		})
+	})
+	Context("Knight", func() {
+		Context("8x8", func() {
+			Context("Knight on b1", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(8)
+					board.PlacePiece("K", "b1")
+					Expect(board.Moves()).To(ConsistOf("a3", "c3", "d2"))
+				})
+			})
+			Context("Knight on c4", func() {
+				It("returns correct positions", func() {
+					board := NewBoardOfSize(8)
+					board.PlacePiece("K", "c4")
+					Expect(board.Moves()).To(ConsistOf("e5", "d6", "d2", "e3", "b2", "a3", "a5", "b6"))
 				})
 			})
 		})
